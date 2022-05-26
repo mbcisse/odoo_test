24672862449abc1594199f65404240fa65dfa868
mbcisse <mohamedbassirou1989@gmail.com>
2022-05-26 13:45:40 +0200

    com1

--- odoo.py
+++ odoo.py
@@ -7,3 +7,11 @@ common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
 print(common.version())
 
 uid = common.authenticate(db, username, password, {})
+models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+control= models.execute_kw(db, uid, password, 'res.partner', 'check_access_rights', ['read'], {'raise_exception': False})
+record1= models.execute_kw(db, uid, password, 'res.partner', 'search', [[['is_company', '=', True]]])
+ids = models.execute_kw(db, uid, password, 'res.partner', 'search', [[['is_company', '=', True]]], {'limit': 1})
+
+res1= models.execute_kw(db, uid, password, 'res.partner', 'read', [ids], {'fields': ['name', 'country_id', 'comment']})
+
+print(res1)
